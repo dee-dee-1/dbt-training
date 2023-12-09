315a01f7a6604e9cc710077f435f79ab67236b89
deepak81188@gmail.com <deepak81188@gmail.com>
2023-12-09 09:35:15 +0000

    Developed report_profit_by_customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -7,10 +7,12 @@ o.ordersellingprice,
 o.ordercostprice,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 -- from raw product
+p.productid,
 p.category,
 p.subcategory,
 p.productname
